66982ea0e42a0b156e0fa42d5d1d4d843c0299d4
Walter Shewmake <25shewmwalj@washk12.org>
2021-01-30 21:41:03 -0700

    asdasd

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let myCodeMirror = CodeMirror.fromTextArea(document.getElementById('editor'), {
     lineNumbers: true
 });
+$(document).on('click','.button', (event)=>{
+    var target = event.target;
+    s = event.target;
+    if (event.target.toString().includes("Heading") || event.target.toString().includes("Image")) {
+        target = event.target.parentElement;
+    } 
+    SelectButton(target.id);
+});
 
-$(document).on('click','.button', ()=>{
-    console.log($(this));
-})
+function SelectButton(id) {
+
+}
